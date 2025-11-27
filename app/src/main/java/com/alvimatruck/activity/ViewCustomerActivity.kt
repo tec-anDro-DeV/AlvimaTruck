4bf74641fb0fa4a579e857500f14190036a64c9b
Kaivan <Kaivan.techcronus@gmail.com>
2025-11-27 12:09:42 +0530

    - Updated Home screen dashboard to include Today's Visit, Total Credit, and Total Cash sections - Refactored View Customer screen UI, replacing "Skip" with "Visited" and repositioning "New Order" - Implemented logic for "Visited" and "New Order" actions in View Customer screen - Renamed and updated selection dialog to "Visit Done" dialog with specific business reasons and comment input - Applied text capitalization and formatting updates across Report, Fleet, Transfer, and Operations screens

--- app/src/main/java/com/alvimatruck/activity/ViewCustomerActivity.kt
+++ app/src/main/java/com/alvimatruck/activity/ViewCustomerActivity.kt
@@ -2,6 +2,10 @@ package com.alvimatruck.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.alvimatruck.R
 import com.alvimatruck.custom.BaseActivity
 import com.alvimatruck.databinding.ActivityViewCustomerBinding
 
@@ -19,6 +23,37 @@ class ViewCustomerActivity : BaseActivity<ActivityViewCustomerBinding>() {
 
         binding.btnEdit.setOnClickListener {
             startActivity(Intent(this, UpdateCustomerActivity::class.java))
+        }
+
+        binding.tvNewOrder.setOnClickListener {
+            startActivity(Intent(this, NewSalesActivity::class.java))
+        }
+
+        binding.tvVisited.setOnClickListener {
+            val inflater = layoutInflater
+            val alertLayout = inflater.inflate(R.layout.dialog_visited, null)
+
+            val btnCancel = alertLayout.findViewById<TextView>(R.id.btnCancel)
+            val btnSubmit = alertLayout.findViewById<TextView>(R.id.btnSubmit)
+
+
+            val dialog = AlertDialog.Builder(this)
+                .setView(alertLayout)
+                .setCancelable(false)
+                .create()
+            dialog.window?.setBackgroundDrawableResource(R.drawable.dialog_background)
+
+
+            btnCancel.setOnClickListener {
+                dialog.dismiss()
+            }
+            btnSubmit.setOnClickListener {
+                dialog.dismiss()
+            }
+            dialog.show()
+            val width =
+                (resources.displayMetrics.widthPixels * 0.9).toInt() // 80% of screen width
+            dialog.window?.setLayout(width, WindowManager.LayoutParams.WRAP_CONTENT)
         }
     }
 }
