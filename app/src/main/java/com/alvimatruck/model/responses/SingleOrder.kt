cbda49bbf3ee1681b693745dd47f9d67c8eed099
Kaivan <Kaivan.techcronus@gmail.com>
2025-12-17 19:33:36 +0530

    - Added helper methods `finalPrice()` and `singleUnitPriceWithTax()` to `SingleOrder` model for formatted decimal output - Updated `SingleOrder` to import `to2Decimal` utility function

--- app/src/main/java/com/alvimatruck/model/responses/SingleOrder.kt
+++ app/src/main/java/com/alvimatruck/model/responses/SingleOrder.kt
@@ -1,5 +1,7 @@
 package com.alvimatruck.model.responses
 
+import com.alvimatruck.utils.Utils.to2Decimal
+
 data class SingleOrder(
     var finalPrice: Double,
     var itemName: String,
@@ -8,4 +10,12 @@ data class SingleOrder(
     var unitPrice: Double,
     var vat: Double,
     var unitOfMeasure: String
-)
+) {
+    fun finalPrice(): String {
+        return finalPrice.to2Decimal()
+    }
+
+    fun singleUnitPriceWithTax(): String {
+        return (unitPrice + vat).to2Decimal()
+    }
+}
